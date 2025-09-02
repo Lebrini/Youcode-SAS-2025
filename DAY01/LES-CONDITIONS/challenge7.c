c77689c72469f2c566e49ebef321fad881b84eed
Lebrini <aminelebrini32@gmail.com>
2025-09-02 17:31:08 +0200

    j'ai cree un variable char pour saisir un caractere pour il verifie s'il trouve dans l'intervalle de 65 a 90 pour les caractere en majuscule selon code ascii puis imprimee le resultat

---
+++ DAY01/LES-CONDITIONS/challenge7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    
+   char x;
+   
+   printf("entrer un caractere : ");
+   scanf("%c",&x);
+   
+   if(x >= 65 && x <= 90)
+   {
+       printf("le caractere est en majuscule");
+   }else
+   {
+       printf("le caractere est en minuscule");
+   }
+    return 0;
+}
