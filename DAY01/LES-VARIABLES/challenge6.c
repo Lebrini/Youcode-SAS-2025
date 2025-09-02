ac153ba90309b7e3d67da0a0c7028084d51f777a
Lebrini <aminelebrini32@gmail.com>
2025-09-02 13:08:16 +0200

    j'ai cree 2 variables a et b et calcul de somme et soustraction et produit par des relation saisi dans printf

---
+++ DAY01/LES-VARIABLES/challenge6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a,b;
+    
+    printf("entrer la valeur de a : ");
+    scanf("%d",&a);
+    printf("entrer la valeur de b : ");
+    scanf("%d",&b);
+    
+    printf("la somme de a et b : %d\n", a + b);
+    printf("la soustraction entre a et b : %d\n", a - b);
+    printf("le produit de a et b : %d\n", a * b);
+    printf("la division de a et b : %d\n", a / b);
+    return 0;
+}
