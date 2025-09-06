4020490bea1082708e424d7d6c81bac64fb9ef98
AMINE LEBRINI <71144690+Lebrini@users.noreply.github.com>
2025-09-06 16:37:57 +0100

    J'ai créé un programme C qui supprime les espaces d'une chaîne de caractères.  D'abord j'ai déclaré un tableau str et lu la chaîne saisie par l'utilisateur avec fgets.  Ensuite j'ai parcouru la chaîne caractère par caractère avec une boucle while.  À chaque itération, si le caractère n'est pas un espace, je le recopie dans la nouvelle position avec l'index j.  Sinon je l'ignore pour le supprimer.  À la fin, j'ajoute le caractère de fin '\\0' pour terminer correctement la chaîne.  Enfin, j'affiche la chaîne résultante sans espaces.

---
+++ DAY03/STRINGS/challenge9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    int i = 0, j = 0;
+    while (str[i])
+    {
+        if(str[i] != ' ')
+        {
+            str[j] = str[i];
+            j++;
+        }
+        i++;
+    }
+    str[j] = '\0';
+    printf("String after removing spaces: %s\n", str);
+    
+    return 0;
+}
